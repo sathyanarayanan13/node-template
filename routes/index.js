624fccee3e2a1a58f5e68174c3fe0a47c0586188
sathyanarayanan13 <sathya.naran05@gmail.com>
2024-04-22 13:11:32 +0000

    create table

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,26 @@ router.get('/time', async function(req, res, next) {
   }
 });
 
+router.get('/setup', async (req, res) => {
+    try {
+      const createTableQuery = `
+        CREATE TABLE schools (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(100),
+          address VARCHAR(100)
+        );
+      `;
+      await db.none(createTableQuery);
+      res.status(200).send({ message: "Successfully created table" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
 // index page
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render("index", {title : "Docker sample", students: []})
 });
 
 // 404 fallback
